ef0752fc527ae8069aa1c513d04cfb44ed058079
DavioCZ <david.kucera.ai@gmail.com>
2025-08-14 17:15:45 +0200

    chore: clean up repo and update .gitignore

--- run_npm.ps1
+++
@@ -1,7 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-# Přidání cesty k Node.js do PATH pro tuto session
-$nodePath = "D:\OneDrive - SATRA, spol. s r.o\PRIV\VSC\Node.js\node-v22.16.0-win-x64"
-$env:Path = "$nodePath;" + $env:Path
-
-# Spuštění npm, které nyní najde node a další příkazy
-npm run dev
